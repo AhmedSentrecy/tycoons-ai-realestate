6a3c966ab2c242e537262f84b48ebfeb6e024565
ahmed SenTRecY <sentrecy@gmail.com>
2026-07-10 01:58:51 +0300

    Trigger voice patch after build script

---
+++ shutil.py
@@ -0,0 +1,11 @@
+import atexit
+
+
+def apply_voice_patch_after_build():
+    try:
+        import patch_voice_direct_agent  # noqa: F401
+    except Exception as exc:
+        print('[Tycoons] direct ElevenLabs voice patch skipped:', exc)
+
+
+atexit.register(apply_voice_patch_after_build)
