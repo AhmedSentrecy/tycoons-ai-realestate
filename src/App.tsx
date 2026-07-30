02e0601367eb166f6465c7a6af76ee11e818ba89
ahmed SenTRecY <sentrecy@gmail.com>
2026-07-30 16:06:30 +0300

    Route direct project links in React app

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 
 const RegionPage = lazy(() => import('./pages/RegionPage'))
+const ProjectPage = lazy(() => import('./pages/ProjectPage'))
 const FaqPage = lazy(() => import('./pages/FaqPage'))
 const AboutPage = lazy(() => import('./pages/AboutPage'))
 
@@ -17,6 +18,7 @@ export default function App() {
     >
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects/:slug" element={<ProjectPage />} />
         <Route path="/regions/:slug" element={<RegionPage />} />
         <Route path="/faq" element={<FaqPage />} />
         <Route path="/about" element={<AboutPage />} />
